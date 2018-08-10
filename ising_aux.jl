ea2b6276b13034f003c031b2f55e94e528f68e9b
guruvamsi-policharla <guruvamsi.policharla@gmail.com>
2018-08-10 00:41:31 -0700

    Revert "Efficiency improvemetns in the Heisenberg Model"
    This reverts commit af26c4d6992f6c29742bf0485b54e1b82d920b52.

--- ising_aux.jl
+++ ising_aux.jl
@@ -1,18 +1,19 @@
 function initialise(N::Int)
     """ Initialising the lattice with random values """
-    #lat = Array{Float64, 3}(N, N)
-    lat = Array{Vector{Float64},2}(N, N);
+lat = zeros(N,N)
     for i = 1:N
         for j = 1:N
-            x = [rand(Uniform(0,1)), rand(Uniform(0,1)), rand(Uniform(0,1))]
-            x = x/sqrt(vecdot(x,x))
-            lat[i,j] = convert(Vector,x)
+            if (rand()>=0.5)
+                lat[i,j] = 1
+            else
+                lat[i,j] = -1
+            end
         end
     end
     return lat
 end
 
-function energy_pos(x, y, lat, a = [0,0,0])
+function energy_pos(x, y, lat)
     """ Calculating energy of the position """
     N = size(lat,1)
     if(y==N)
@@ -39,52 +40,34 @@ function energy_pos(x, y, lat, a = [0,0,0])
         right=x+1;
     end
 
-    if a == [0,0,0]
-        energy = -1*vecdot(lat[x,y],(lat[left,y]+lat[right,y]+lat[x,up]+lat[x,down]));
-        return energy
-    else
-        energy = -1*vecdot(a,(lat[left,y]+lat[right,y]+lat[x,up]+lat[x,down]));
-        return energy
-    end
+    energy = -1*lat[x,y]*(lat[left,y]+lat[right,y]+lat[x,up]+lat[x,down]);
+    return energy
 end
 
 function test_flip(x, y, lat, T)
 """ Checks whether energy allows for a flip or not """
-    theta = rand(Uniform(0,2*pi))
-    phi = rand(Uniform(0,2*pi))
-    a = convert(Vector,[sin(theta)*cos(phi),sin(theta)*sin(phi),cos(theta)])
-    de = -energy_pos(x,y,lat) + energy_pos(x,y,lat,a);
+    de = -energy_pos(x,y,lat);
 
     if(de<0)
-        lat[x,y] = a
-        return true
-    elseif(rand()<exp(-de/T))
-        lat[x,y] = a
-        return true
+        flip = true;
+    elseif(rand()<exp(-2*de/T))
+        flip = true;
     else
-        return false
+        flip = false;
     end
+    return flip
 end
-
-'''
-function flip(x,y,lat)
-    N = size(lat,1)
-    theta = rand(Uniform(0,2*pi))
-    phi = rand(Uniform(0,2*pi))
-
-    #Flipping randomly
-    lat[x,y] = [sin(theta)*cos(phi),sin(theta)*sin(phi),cos(theta)]
-end
-'''
 
 function transient_results(lat, transient::Int, T)
     """Takes lat as input and removes initial transients by running for transient number of steps"""
     N = size(lat,1)
     for i = 1:transient
         for j = 1:N*N
-                x = rand(1:N)
-                y = rand(1:N)
-                test_flip(x,y,lat,T)
+            x = rand(1:N)
+            y = rand(1:N)
+            if(test_flip(x,y,lat,T) == true)
+                lat[x,y] = -lat[x,y];
+            end
         end
     end
 end
